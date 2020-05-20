23a5892c361f5f5571626ba017c7684dc7dfd457
Lucas <me@dotronglong.com>
2020-05-20 20:37:21 +0800

    Add Cause to HTTP Error

--- error_http.go
+++ error_http.go
@@ -14,27 +14,34 @@ type HttpError interface {
 	WithMessage(message string) HttpError
 	Status() int
 	WithStatus(status int) HttpError
+	Cause() error
+	WithCause(cause error) HttpError
 }
 
+// NewHttpError returns a HttpError
 func NewHttpError(code string, message string, status int) HttpError {
-	return &factoryHttpError{
+	return &factoryHTTPError{
 		code:    code,
 		message: message,
 		status:  status,
 	}
 }
 
-type factoryHttpError struct {
+type factoryHTTPError struct {
 	code    string
 	message string
 	status  int
+	cause   error
 }
 
-func (e *factoryHttpError) Error() string {
+func (e *factoryHTTPError) Error() string {
+	if e.cause != nil {
+		return strings.TrimSpace(fmt.Sprintf("[%s] %s. Cause: %s", e.code, e.message, e.cause.Error()))
+	}
 	return strings.TrimSpace(fmt.Sprintf("[%s] %s", e.code, e.message))
 }
 
-func (e *factoryHttpError) MarshalJSON() ([]byte, error) {
+func (e *factoryHTTPError) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
 		Code    string `json:"code"`
 		Message string `json:"message"`
@@ -44,29 +51,38 @@ func (e *factoryHttpError) MarshalJSON() ([]byte, error) {
 	})
 }
 
-func (e *factoryHttpError) Code() string {
+func (e *factoryHTTPError) Code() string {
 	return e.code
 }
 
-func (e *factoryHttpError) WithCode(code string) HttpError {
+func (e *factoryHTTPError) WithCode(code string) HttpError {
 	e.code = code
 	return e
 }
 
-func (e *factoryHttpError) Message() string {
+func (e *factoryHTTPError) Message() string {
 	return e.message
 }
 
-func (e *factoryHttpError) WithMessage(message string) HttpError {
+func (e *factoryHTTPError) WithMessage(message string) HttpError {
 	e.message = message
 	return e
 }
 
-func (e *factoryHttpError) Status() int {
+func (e *factoryHTTPError) Status() int {
 	return e.status
 }
 
-func (e *factoryHttpError) WithStatus(status int) HttpError {
+func (e *factoryHTTPError) WithStatus(status int) HttpError {
 	e.status = status
+	return e
+}
+
+func (e *factoryHTTPError) Cause() error {
+	return e.cause
+}
+
+func (e *factoryHTTPError) WithCause(cause error) HttpError {
+	e.cause = cause
 	return e
 }
